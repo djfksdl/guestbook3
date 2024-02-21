9193825a6464fed029b168cf6b2de4a9f6254eec
djfksdl <djfksdl@naver.com>
2024-02-21 18:39:14 +0900

    insert,select

--- src/main/java/com/javaex/controller/GuestbookController.java
+++ src/main/java/com/javaex/controller/GuestbookController.java
@@ -1,40 +1,74 @@
 package com.javaex.controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class GuestbookController
- */
-@WebServlet("/GuestbookController")
+import com.javaex.dao.GuestDao;
+import com.javaex.util.WebUtil;
+import com.javaex.vo.GuestVo;
+
+@WebServlet("/gbc")
 public class GuestbookController extends HttpServlet {
+	//필드
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public GuestbookController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	//생성자
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    //메소드-일반
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("GuestbookController.goGet()");//실행확인
+		
+		String action = request.getParameter("action");
+		System.out.println(action);
+		
+		if("alform".equals(action)) { //메인화면
+			System.out.println("alform:메인화면");
+			
+			//DB관련
+			GuestDao guestDao = new GuestDao();
+			
+			//리스트 불러오기
+			List<GuestVo> guestList = guestDao.guestSelect();
+			
+			//데이터 담기
+			request.setAttribute("guestList", guestList);
+			
+			//포워드
+			WebUtil.forward(request, response, "/addList.jsp");
+			
+		}else if("insert".equals(action)) {//등록
+			System.out.println("alform:등록");
+			
+			//값 저장하기
+			String name= request.getParameter("name");
+			String pw= request.getParameter("pw");
+			String content= request.getParameter("content");
+			String reg_date = request.getParameter("reg_date");
+			
+			//Vo로 묶기
+			GuestVo guestVo = new GuestVo(name, pw, content, reg_date);
+			
+			//DB관련연결
+			GuestDao guestDao = new GuestDao();
+			
+			//Dao메소드로 연결
+			guestDao.guestInsert(guestVo);
+			
+			//리다이렉트
+			WebUtil.redirect(request, response, "/guestbook3/gbc?action=alform");
+			
+			
+		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
--- src/main/java/com/javaex/dao/GuestDao.java
+++ src/main/java/com/javaex/dao/GuestDao.java
@@ -1,5 +1,149 @@
 package com.javaex.dao;
 
-public class GuestDao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-}
+import com.javaex.vo.GuestVo;
+
+public class GuestDao {//DB관련
+	
+	//필드-없음
+	//생성자
+	//메소드-gs
+	//메소드-일반
+	
+	//전체가져오기
+	public List<GuestVo> guestSelect() {
+		List<GuestVo> guestList = new ArrayList<GuestVo>();
+		
+		// 0. import java.sql.*;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+		// 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+		// 2. Connection 얻어오기
+			String url = "jdbc:mysql://localhost:3306/guestbook_db";
+			conn = DriverManager.getConnection(url, "guestbook", "guestbook");
+			
+		// 3. SQL문 준비 / 바인딩 / 실행
+			//SQL문 준비
+			String query="";
+			query +=" select no ";
+			query +=" ,name ";
+			query +=" ,password ";
+			query +=" ,content ";
+			query +=" ,reg_date ";
+			query +=" from guestbook ";
+
+			//바인딩
+			pstmt = conn.prepareStatement(query);
+			
+			//실행
+			rs = pstmt.executeQuery(); //select문 빼고 나머지는 executeUpdate()씀!
+			
+		// 4.결과처리
+			while(rs.next()) {
+				int no = rs.getInt("no");
+				String name = rs.getString("name");
+				String pw =rs.getString("password");
+				String content = rs.getString("content");
+				String reg_date = rs.getString("reg_date");
+				
+				//d에서 가져온 데이터 vo로 묶기
+				GuestVo guesetVo = new GuestVo(no, name, pw, content, reg_date);
+				guestList.add(guesetVo);
+				
+			}
+			
+			
+		} catch (ClassNotFoundException e) {
+		System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+		System.out.println("error:" + e);
+		} finally {
+		// 5. 자원정리
+		try {
+		if (rs != null) {
+		rs.close();
+		} 
+		if (pstmt != null) {
+		pstmt.close();
+		}
+		if (conn != null) {
+		conn.close();
+		}
+		} catch (SQLException e) {
+		System.out.println("error:" + e);
+		}
+		}
+		return guestList;
+		
+	}
+	
+	//등록
+	public int guestInsert(GuestVo guestVo) {
+		int count = -1;
+		// 0. import java.sql.*;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+		// 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+		// 2. Connection 얻어오기
+			String url = "jdbc:mysql://localhost:3306/guestbook_db";
+			conn = DriverManager.getConnection(url, "guestbook", "guestbook");
+			
+		// 3. SQL문 준비 / 바인딩 / 실행
+			//SQL문 준비
+			String query="";
+			query +=" insert into guestbook  ";
+			query +=" values(null, ?,?,?,now()) ";
+			
+
+			//바인딩
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1,guestVo.getName());
+			pstmt.setString(2, guestVo.getPw());
+			pstmt.setString(3, guestVo.getContent());
+			
+			//실행
+			count = pstmt.executeUpdate(); //select문 빼고 나머지는 executeUpdate()씀!
+			
+		// 4.결과처리
+			System.out.println("1건 등록되었습니다.");
+			
+			
+		} catch (ClassNotFoundException e) {
+		System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+		System.out.println("error:" + e);
+		} finally {
+		// 5. 자원정리
+		try {
+		if (rs != null) {
+		rs.close();
+		} 
+		if (pstmt != null) {
+		pstmt.close();
+		}
+		if (conn != null) {
+		conn.close();
+		}
+		} catch (SQLException e) {
+		System.out.println("error:" + e);
+		}
+		}
+		return count;
+	}
+		
+}//Dao끝
